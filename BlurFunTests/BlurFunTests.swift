45eddcc5632b1a9a60c2c33342095ec0005d0a2a
iKilledAppl3 <ikilledappl3@nsts-imac-4.local>
2016-07-11 20:04:22 -0400

    Initial Commit

---
+++ BlurFunTests/BlurFunTests.swift
@@ -0,0 +1,36 @@
+//
+//  BlurFunTests.swift
+//  BlurFunTests
+//
+//  Created by iKilledAppl3 on 7/11/16.
+//  Copyright © 2016 iKilledAppl3. All rights reserved.
+//
+
+import XCTest
+@testable import BlurFun
+
+class BlurFunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
